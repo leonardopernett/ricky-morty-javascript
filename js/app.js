b39f2b8f5e5454b2b15c8397d633c0a148c6f6b0
leonardo pernett <pernettleonardo@gmail.com>
2020-06-28 11:19:03 -0500

    add function javascript

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 import API from "./api.js";
 import Character from './character.js';
 
+// const btn = document.querySelector('#btn');
+// let count = 0;
 const api = new API();
+
 
 const init = async (id)=>{
     const result = await  api.getCharacter(id)
-    new Character(result);
     console.log(result)
+    new Character(result);
 }
 
-init(3)
+// btn.addEventListener('click',async ()=>{
+//     count ++;
+//     let response = await  api.getCharacter(count)
+//     new Character(response);
+// })
+
+init(1)
--- js/character.js
+++ js/character.js
@@ -1,27 +1,31 @@
-
 export default class Character {
-  constructor({ name, image }) {
+  
+  constructor({ name, image, gender, species, status }) {
     this.characterContainer = document.querySelector("#character-container");
     this.name = name;
     this.image = image;
+    this.gender = gender;
+    this.species = species;
+    this.status = status;
     this.render();
   }
 
   build() {
-    return `
-        <article class="character">
-          <div class="character-grip">
-            <h2>${this.name}</h2>
-            <img src=${this.image} >   
-          </div>
-        </article>
+    return /*html*/ `
+    <article class="character">
+      <div class="character-grid">
+        <img class="character-image" src="${this.image}"/>
+        <h2 class="character-name">${this.name}</h2>
+        <div class="character-labels">
+            <h3 class="character-label">Genero: ${this.gender}</h3>
+            <h3 class="character-label">Especie: ${this.species}</h3>
+            <h3 class="character-label">Status:  ${this.status}</h3>
+        </div>
+      </div>
+    </article>
     `;
   }
   render() {
-    if(this.characterContainer ===null){
-        return console.log('null')
-    }else {
-        return this.characterContainer.innerHTML = this.build();
-    }
+    return (this.characterContainer.innerHTML = this.build());
   }
 }
